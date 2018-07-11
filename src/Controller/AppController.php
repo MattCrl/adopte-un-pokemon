fc1f532aeab7e674836dfca3c88b75841ec401d2
Matt Crl <matth.carlevaris@gmail.com>
2018-07-11 21:10:44 +0200

    add npm dependencies

---
+++ src/Controller/AppController.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mattcrl
+ * Date: 11/07/18
+ * Time: 20:59
+ */
+
+namespace App\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class AppController extends Controller
+{
+
+    /**
+     * Homepage
+     *
+     * @Route("/", name="app_index")
+     * @Method("GET")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction()
+    {
+        return $this->render('index.html.twig');
+    }
+
+}
