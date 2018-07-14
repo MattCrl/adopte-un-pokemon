71ac823bb3a967cc8bffae001c7af66ddbafb2a6
Matt Crl <matth.carlevaris@gmail.com>
2018-07-14 13:53:47 +0200

    Data transform done

--- src/Form/AdType.php
+++ src/Form/AdType.php
@@ -3,14 +3,26 @@
 namespace App\Form;
 
 use App\Entity\Ad;
+use Doctrine\Common\Collections\ArrayCollection;
+use App\Form\DataTransformer\StringToPokemonTransformer;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class AdType extends AbstractType
 {
+    private $em;
+
+    private $transformer;
+
+    public function __construct(EntityManagerInterface $em, StringToPokemonTransformer $transformer)
+    {
+        $this->em = $em;
+        $this->transformer = $transformer;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -23,10 +35,13 @@ class AdType extends AbstractType
             ->add('category')
             ->add('pokemon',
                         TextType::class,
-                            ['attr' => ['autocomplete' => 'off']])
-            ->add('submit',
-                            SubmitType::class)
-        ;
+                            ['attr' => ['autocomplete' => 'off']]);
+
+        $builder
+            ->get('pokemon')
+            ->addModelTransformer($this->transformer);
+
+
     }
 
     public function configureOptions(OptionsResolver $resolver)
@@ -35,4 +50,7 @@ class AdType extends AbstractType
             'data_class' => Ad::class,
         ]);
     }
+
+
+
 }
