f3ae1df313c62fe66d58215037e7da197a2b859c
Matt Crl <matth.carlevaris@gmail.com>
2018-07-24 11:42:17 +0200

    Favorites list done

--- src/Controller/FavoriteController.php
+++ src/Controller/FavoriteController.php
@@ -5,10 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route("/favorite")
- */
+
 class FavoriteController extends Controller
 {
+    /**
+     * @Route("/member/favorites", name="app_member_fav")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction()
+    {
+        $favorites = $this->getUser()->getFavorites();
+
+        $user= $this->getUser();
+
+        return $this->render('favorite/index.html.twig', [
+            'user' => $user,
+            'favorites' => $favorites
+        ]);
+    }
 
 }
